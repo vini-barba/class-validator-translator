d5fec66d1299227963d10d52f3814ba18db2efa4
Barba <vinicius_gomes.94@hotmail.com>
2022-09-16 14:47:04 -0300

    chore - set lint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["import", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "import/extensions": "off",
+    "import/no-unresolved": "error",
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "import/no-extraneous-dependencies": 0,
+    "import/prefer-default-export": "off",
+    "no-underscore-dangle": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "class-methods-use-this": "off",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+    },
+  },
+};
