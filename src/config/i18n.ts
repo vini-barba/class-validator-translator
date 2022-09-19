a11620d07c57c989796234afeac63668a618c0e9
Barba <vinicius_gomes.94@hotmail.com>
2022-09-19 09:41:56 -0300

    feat(i18n): set i18 and implementing the Contains translation

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,7 +1,9 @@
 import { I18n } from 'i18n';
 import { join } from 'path';
+import { langs } from './langs';
 
 export const i18n = new I18n({
   locales: ['pt'],
   directory: join(__dirname, 'locales'),
+  defaultLocale: langs.pt,
 });
