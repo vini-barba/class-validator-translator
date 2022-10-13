7192225bd08a7ccba7aab52afa3aae6855a33d6b
Bruno Bigotto <bbigotto@gmail.com>
2022-10-13 11:38:14 -0300

    feat(length): fixing logic to select sentences

--- src/messages/length/length.ts
+++ src/messages/length/length.ts
@@ -2,17 +2,26 @@ import { ValidationArguments } from 'class-validator';
 import { i18n } from '../../config/i18n';
 
 export default function Length(args: ValidationArguments) {
-  return i18n.__(
-    // eslint-disable-next-line no-nested-ternary
-    args.value.length < args.constraints[0]
-      ? 'LengthMin'
-      : args.value.length > args.constraints[1]
-      ? 'LengthMax'
-      : 'LenthBeetween',
-    {
+  const isMinLength =
+    args.constraints[0] !== null && args.constraints[0] !== undefined;
+  const isMaxLength =
+    args.constraints[1] !== null && args.constraints[1] !== undefined;
+  if (isMinLength && (!args.value || args.value.length < args.constraints[0])) {
+    return i18n.__('LengthMin', {
       property: args.property,
-      constraint1: args.constraints[0],
-      constraint2: args.constraints[1],
-    },
-  );
+      constraint: args.constraints[0],
+    });
+  }
+  if (isMaxLength && args.value.length > args.constraints[1]) {
+    return i18n.__('LengthMax', {
+      property: args.property,
+      constraint: args.constraints[1],
+    });
+  }
+
+  return i18n.__('LengthEach', {
+    property: args.property,
+    constraint1: args.constraints[0],
+    constraint2: args.constraints[1],
+  });
 }
